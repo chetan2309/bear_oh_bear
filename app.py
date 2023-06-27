24f0d0f6ea9f3e68c8e3f6bebf0341d3a3b9fa99
Chetan Sharma <chetan@Chetans-MacBook-Air.local>
2023-06-27 17:26:18 +0530

    Uploaded the model

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+__all__ = ["is_cat", "learn", "categories", "classify_images", "image", "label", "examples", "intf"]
+
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_cat(x): return x[0].isupper()
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+learn  = load_learner('model.pk1')
+
+categories = ('Grizzly', 'Teddy', 'Polar', 'Black')
+
+def classify_image(img):
+    predict,index,probs = learn.predict(img)
+    return dict(zip(categories, map(float, probs)))
+
+image = gr.input.Image(shape=(192,192))
+label = gr.outputs.Label()
+examples = ['grizzly.jpg', 'black.jpg', 'polar.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
