0b0759afa1e38e722673f69f6fd5257f4f479d62
Chetan Sharma <chetan@Chetans-MacBook-Air.local>
2023-06-27 19:22:10 +0530

    Made small adjustments

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ def is_cat(x): return x[0].isupper()
 
 learn  = load_learner('model.pkl')
 
-categories = ('Grizzly', 'Teddy', 'Polar', 'Black')
+#categories = ('Grizzly', 'Teddy', 'Polar', 'Black')
 
+labels = learn.dls.vocab
 def classify_image(img):
-    predict,index,probs = learn.predict(img)
-    return dict(zip(categories, map(float, probs)))
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
 image = gr.inputs.Image(shape=(192,192))
-label = gr.outputs.Label()
-examples = ['grizzly.jpeg', 'teddy.jpeg', 'polar.jpeg', 'black.jpeg']
+label = gr.outputs.Label(num_top_classes=4)
+#examples = ['grizzly.jpeg', 'teddy.jpeg', 'polar.jpeg', 'black.jpeg']
 
-intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label)
 intf.launch(inline=False)
